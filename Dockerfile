d82d55800c9f8a9185b3fa5faf28bd2534a61045
jc-773 <jonc960@gmail.com>
2025-07-27 20:53:42 -0400

    update app for mvn container

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM eclipse-temurin:23-jdk-alpine
 WORKDIR /app
-COPY target/*.jar app.jar
+COPY target/news_agent-0.0.1-SNAPSHOT.jar app.jar
 ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/latest/news_agent/NewsAgentApplication.java
+++ src/main/java/com/latest/news_agent/NewsAgentApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.latest.news_agent.service.CronServiceInit;
