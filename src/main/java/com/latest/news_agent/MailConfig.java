906572fd9410b683be27695f76902563584f281b
jc-773 <jonc960@gmail.com>
2025-07-27 21:08:38 -0400

    create java mail bean

---
+++ src/main/java/com/latest/news_agent/MailConfig.java
@@ -0,0 +1,26 @@
+package com.latest.news_agent;
+
+import java.util.Properties;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class MailConfig {
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(System.getenv("SPRING_MAIL_HOST"));
+        mailSender.setPort(Integer.parseInt(System.getenv("SPRING_MAIL_PORT")));
+        mailSender.setUsername(System.getenv("SPRING_MAIL_USERNAME"));
+        mailSender.setPassword(System.getenv("SPRING_MAIL_PASSWORD"));
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        return mailSender;
+    }
+}
